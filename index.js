05b76b350a9714b2b5b024bf767e1c061590a19b
Kesha <kesha.marges@gmail.com>
2022-12-19 19:12:57 -0500

    commit tree 3487539c543d0bed461167d2b59b0cae397b7396

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solution here
+function findMatching(array, str) {
+    return array.filter(function (driver) {
+        return driver.toUpperCase() === str.toUpperCase();
+    });
+};
+
+function fuzzyMatch(array, str1) {
+    return array.filter(function (driver) {
+        return driver.startsWith(str1)
+    });
+};
+
+function matchName(array, str3) {
+    return array.filter(function (obj) {
+        return obj.name === str3;
+    });
+}
